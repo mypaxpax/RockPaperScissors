0cae97d3a30b43ac09f8653227f0a80e300615e2
mypaxpax <thomasgade@gmail.com>
2022-04-27 22:59:30 +0200

    Added some documentation.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 let cpuWin = 0;
 let playerWin = 0;
 
+// Pick the elements from the html and assign them to variable.
 const playerRock = document.querySelector("#buttonRock");
 const playerPaper = document.querySelector("#buttonPaper");
 const playerScissors = document.querySelector("#buttonScissors");
 const results = document.querySelector("#resultDiv");
+results.textContent = `Current score: Player: ${playerWin} - ${cpuWin} CPU`;
 
+// Random pick 1 choice from an array consisting of 3 possible picks.
 function computerPlay() {
   const picks = ["rock", "paper", "scissors"];
   const randomPick = picks[Math.floor(Math.random() * picks.length)];
   return randomPick;
 }
 
+// Run playRound() with the chosen pick of rock, paper or scissors.
 function start() {
   playerRock.addEventListener("click", () => {
     playRound("rock");
@@ -24,10 +28,12 @@ function start() {
   });
 }
 
+// Takes the input from start() function buttons and add it as playerChoices.
 function playRound(pChoice) {
   const playerSelection = pChoice;
   const computerSelection = computerPlay();
 
+  // The possible winning outcomes
   switch (playerSelection + computerSelection) {
     case "rockscissors":
     case "paperrock":
@@ -37,14 +43,16 @@ function playRound(pChoice) {
       console.log(`WIN! Player picked ${playerSelection} and CPU picked ${computerSelection}`);
       break;
 
+    // The possible losing outcomes.
+    case "scissorsrock":
     case "rockpaper":
     case "paperscissors":
-    case "scissorsrock":
       ++cpuWin;
       results.textContent = `Current score: Player: ${playerWin} - ${cpuWin} CPU`;
       console.log(`Lost! Player picked ${playerSelection} and CPU picked ${computerSelection}`);
       break;
 
+    // The possible draw outcomes.
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
@@ -52,6 +60,12 @@ function playRound(pChoice) {
       break;
 
     default:
+  }
+
+  if (playerWin === 5) {
+    results.textContent = "Player wins!";
+  } else if (cpuWin === 5) {
+    results.textContent = "CPU wins!";
   }
 }
 start();

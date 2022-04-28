6f6ecbc0d9e5b245ef05110b0f0db850954b1f5a
mypaxpax <thomasgade@gmail.com>
2022-04-28 12:08:55 +0200

    winner, loser and draw functions added.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,73 @@
-let cpuWin = 0;
-let playerWin = 0;
+let cpuScore = 0;
+let playerScore = 0;
 
 // Pick the elements from the html and assign them to variable.
 const playerRock = document.querySelector("#buttonRock");
 const playerPaper = document.querySelector("#buttonPaper");
 const playerScissors = document.querySelector("#buttonScissors");
 const results = document.querySelector("#resultDiv");
-results.textContent = `Current score: Player: ${playerWin} - ${cpuWin} CPU`;
+results.textContent = `Current score: Player: ${playerScore} - ${cpuScore} CPU`;
 
 // Random pick 1 choice from an array consisting of 3 possible picks.
 function computerPlay() {
   const picks = ["rock", "paper", "scissors"];
-  const randomPick = picks[Math.floor(Math.random() * picks.length)];
-  return randomPick;
+  const cpuPick = picks[Math.floor(Math.random() * picks.length)];
+  return cpuPick;
+}
+
+function winner() {
+  if (playerScore < 5 && cpuScore < 5) {
+    ++playerScore;
+    results.textContent = `Current score: Player: ${playerScore} - ${cpuScore} CPU`;
+    console.log(`Player picked ${playerSelection} and CPU picked ${computerSelection}`);
+  } if (playerScore === 5 && cpuScore < 5) {
+    console.log("WIN");
+  }
+}
+
+function loser() {
+  if (playerScore < 5 && cpuScore < 5) {
+    ++cpuScore;
+    results.textContent = `Current score: Player: ${playerScore} - ${cpuScore} CPU`;
+    console.log(`Lost! Player picked ${playerSelection} and CPU picked ${computerSelection}`);
+  } if (playerScore < 5 && cpuScore === 5) {
+    console.log("Lost");
+  }
+}
+
+function draw() {
+  console.log(`Draw! Player picked ${playerSelection} and CPU picked ${computerSelection}`);
+}
+
+// Takes the input from start() function buttons and add it as playerChoices.
+function playRound(pChoice) {
+  playerSelection = pChoice;
+  computerSelection = computerPlay();
+
+  // The possible winning outcomes
+  switch (playerSelection + computerSelection) {
+    case "rockscissors":
+    case "paperrock":
+    case "scissorspaper":
+      winner();
+      break;
+
+    // The possible losing outcomes.
+    case "scissorsrock":
+    case "rockpaper":
+    case "paperscissors":
+      loser();
+      break;
+
+    // The possible draw outcomes.
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+      draw();
+      break;
+
+    default:
+  }
 }
 
 // Run playRound() with the chosen pick of rock, paper or scissors.
@@ -28,44 +83,4 @@ function start() {
   });
 }
 
-// Takes the input from start() function buttons and add it as playerChoices.
-function playRound(pChoice) {
-  const playerSelection = pChoice;
-  const computerSelection = computerPlay();
-
-  // The possible winning outcomes
-  switch (playerSelection + computerSelection) {
-    case "rockscissors":
-    case "paperrock":
-    case "scissorspaper":
-      ++playerWin;
-      results.textContent = `Current score: Player: ${playerWin} - ${cpuWin} CPU`;
-      console.log(`WIN! Player picked ${playerSelection} and CPU picked ${computerSelection}`);
-      break;
-
-    // The possible losing outcomes.
-    case "scissorsrock":
-    case "rockpaper":
-    case "paperscissors":
-      ++cpuWin;
-      results.textContent = `Current score: Player: ${playerWin} - ${cpuWin} CPU`;
-      console.log(`Lost! Player picked ${playerSelection} and CPU picked ${computerSelection}`);
-      break;
-
-    // The possible draw outcomes.
-    case "rockrock":
-    case "paperpaper":
-    case "scissorsscissors":
-      console.log(`Draw! Player picked ${playerSelection} and CPU picked ${computerSelection}`);
-      break;
-
-    default:
-  }
-
-  if (playerWin === 5) {
-    results.textContent = "Player wins!";
-  } else if (cpuWin === 5) {
-    results.textContent = "CPU wins!";
-  }
-}
 start();

1d1580f08019c704e88dcc25a4a795980bf1609d
mypaxpax <thomasgade@gmail.com>
2022-05-04 13:30:25 +0200

    CPU picks from array instead of parent elements.

--- js/script.js
+++ js/script.js
@@ -1,105 +1,60 @@
-let cpuScore = 0;
+const possiblePicks = document.querySelector("#fighterBox").children;
+const playerNinja = document.querySelector("#ninja");
+const playerBear = document.querySelector("#bear");
+const playerCowboy = document.querySelector("#cowboy");
+const combatText = document.querySelector("#combatText");
+const Pscore = document.querySelector(".Pscore");
+const Cscore = document.querySelector(".Cscore");
+const endBattle = document.querySelector(".endBattle");
+const replay = document.querySelector(".replay");
+const playersPick = document.querySelector(".leftBox").children;
+const cpuPick = document.querySelector(".rightBox").children;
+
 let playerScore = 0;
+let computerScore = 0;
 
-// Pick the elements from the html and assign them to variable.
-const buttons = document.querySelector("#rps").children;
-const replay = document.querySelector("#buttonReplay");
-const playerRock = document.querySelector("#rock");
-const playerPaper = document.querySelector("#paper");
-const playerScissors = document.querySelector("#scissors");
-const score = document.querySelector("#scoreDiv");
-const gamePicks = document.querySelector("p.gamePicks");
-const endGame = document.querySelector(".endGameMessage");
-
-// function to show the winner - takes playername + win/loss.
-// changes the display from none to block, making it visible.
-function finish(playerName, status) {
-  endGame.textContent = `${playerName} ${status}`;
-  endGame.style.display = "block";
-}
-
-// score text
-const currentScore = () => {
-  score.textContent = `Player: ${playerScore} - ${cpuScore} CPU`;
+currentScore = () => {
+  Pscore.textContent = `${playerScore}`;
+  Cscore.textContent = `${computerScore}`;
 };
 
-currentScore();
-
-// refresh the page to start new game.
-replay.addEventListener("click", () => {
-  window.location.reload();
-});
-
-// Random pick 1 choice from an array consisting of 3 possible picks.
-function computerPlay() {
-  const picks = ["rock", "paper", "scissors"];
-  const cpuPick = picks[Math.floor(Math.random() * picks.length)];
-  return cpuPick;
-}
-
-// Hides rock, paper, scissors buttons and leave "replay"
-// as the only button left.
-function hideButtons() {
-  buttons[0].style.display = "none";
-  buttons[1].style.display = "none";
-  buttons[2].style.display = "none";
-}
-
-// checks score of player +1 if player wins
-// if player got 5 points, call hideButtons() function.
-function winner() {
-  if (playerScore < 5 && cpuScore < 5) {
-    ++playerScore;
-    currentScore();
-    gamePicks.textContent = `Player picked ${playerSelection} and CPU picked ${computerSelection}`;
-  } if (playerScore === 5 && cpuScore < 5) {
-    finish("Player", "Won");
-    hideButtons();
+computerSelections = () => {
+  const cpuCanPick = ["bear", "cowboy", "ninja"];
+  const computerPick = cpuCanPick[Math.floor(Math.random() * cpuCanPick.length)];
+  if (computerPick === "bear") {
+    console.log(computerPick);
+    cpuPick[1].src = "./images/bear.png";
+  } else if (computerPick === "cowboy") {
+    console.log(computerPick);
+    cpuPick[1].src = "./images/cowboy.png";
+  } else if (computerPick === "ninja") {
+    console.log(computerPick);
+    cpuPick[1].src = "./images/ninja.png";
   }
-}
+  return computerPick;
+};
 
-// checks score of CPU and adds +1 if it wins
-// if CPU got 5 points, call hideButtons() function.
-function loser() {
-  if (playerScore < 5 && cpuScore < 5) {
-    ++cpuScore;
-    currentScore();
-    gamePicks.textContent = `Player picked ${playerSelection} and CPU picked ${computerSelection}`;
-  } if (playerScore < 5 && cpuScore === 5) {
-    finish("CPU", "Won");
-    hideButtons();
-  }
-}
-
-// if player and CPU picks the same.
-function draw() {
-  gamePicks.textContent = `DRAW Player picked ${playerSelection} and CPU picked ${computerSelection}`;
-}
-
-// Takes the input from start() function buttons and add it as playerChoices.
-function playRound(pChoice) {
+function playGame(pChoice) {
   playerSelection = pChoice;
-  computerSelection = computerPlay();
-
-  // The possible winning outcomes
-  switch (playerSelection + computerSelection) {
-    case "rockscissors":
-    case "paperrock":
-    case "scissorspaper":
+  computerChoice = computerSelections();
+  switch (playerSelection + computerChoice) {
+    case "bearninja":
+    case "cowboybear":
+    case "ninjacowboy":
       winner();
       break;
 
     // The possible losing outcomes.
-    case "scissorsrock":
-    case "rockpaper":
-    case "paperscissors":
+    case "ninjabear":
+    case "bearcowboy":
+    case "cowboyninja":
       loser();
       break;
 
     // The possible draw outcomes.
-    case "rockrock":
-    case "paperpaper":
-    case "scissorsscissors":
+    case "bearbear":
+    case "cowboycowboy":
+    case "ninjaninja":
       draw();
       break;
 
@@ -107,19 +62,62 @@ function playRound(pChoice) {
   }
 }
 
-// Run playRound() with the chosen pick of rock, paper or scissors.
-function start() {
-  playerRock.addEventListener("click", () => {
-    playRound("rock");
-  });
-  playerPaper.addEventListener("click", () => {
-    playRound("paper");
-  });
-  playerScissors.addEventListener("click", () => {
-    playRound("scissors");
-  });
-}
+winner = () => {
+  if (playerScore < 5 && computerScore < 5) {
+    ++playerScore;
+    currentScore();
+    combatText.textContent = `Victory! Your ${playerSelection} beat ${computerChoice} with ease!`;
+  } if (playerScore === 5 && computerScore < 5) {
+    finish("Player", "Won");
+    hideButtons();
+  }
+};
 
-currentScore();
+loser = () => {
+  if (playerScore < 5 && computerScore < 5) {
+    ++computerScore;
+    currentScore();
+    combatText.textContent = `Ouch! Your ${playerSelection} got taken out by ${computerChoice}!`;
+  } if (computerScore === 5 && playerScore < 5) {
+    finish("Computer", "Won");
+    hideButtons();
+  }
+};
 
-start();
+hideButtons = () => {
+  possiblePicks[0].style.display = "none";
+  possiblePicks[1].style.display = "none";
+  possiblePicks[2].style.display = "none";
+  replay.style.display = "block";
+};
+
+draw = () => {
+  combatText.textContent = `It's a draw! Looks like you both picked ${playerSelection}'s`;
+};
+
+finish = (playerName, status) => {
+  endBattle.textContent = `We got a winner! ${playerName} ${status}`;
+};
+
+replay.addEventListener("click", () => {
+  location.reload();
+});
+
+gameStart = () => {
+  playerBear.addEventListener("click", () => {
+    playersPick[1].src = "./images/bear.png";
+    playGame("bear");
+  });
+
+  playerNinja.addEventListener("click", () => {
+    playersPick[1].src = "./images/ninja.png";
+    playGame("ninja");
+  });
+
+  playerCowboy.addEventListener("click", () => {
+    playersPick[1].src = "./images/cowboy.png";
+    playGame("cowboy");
+  });
+};
+
+gameStart();
